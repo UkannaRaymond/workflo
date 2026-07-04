a83aa72381f80d8fa8b5145b9a8b49deed871c14
Ukanna <ukannaraymond@gmail.com>
2026-07-04 17:57:50 +0100

    initialiase project with next and ui

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ThemeToggle />
+    </div>
+  );
+}
